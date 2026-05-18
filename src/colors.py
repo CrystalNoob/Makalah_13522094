453974754f923654f26067cff0321b0773e9771e
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2026-05-18 08:52:23 +0700

    style: format source code with black and ruff

--- src/colors.py
+++ src/colors.py
@@ -1,7 +1,7 @@
 class colors:
-	PURPLE = '\033[95m'
-	GREEN = '\033[92m'
-	YELLOW = "\033[93m"
-	RED = "\033[91m"
-	BOLD = "\033[1m"
-	END = "\033[0m"
+    PURPLE: str = "\033[95m"
+    GREEN: str = "\033[92m"
+    YELLOW: str = "\033[93m"
+    RED: str = "\033[91m"
+    BOLD: str = "\033[1m"
+    END: str = "\033[0m"
